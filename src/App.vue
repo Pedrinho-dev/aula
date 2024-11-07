9f605c292765a675a0eaf8727a6cd71158a88ee1
pedrinho-dev <pedroantoniofalves@gmail.com>
2024-11-07 01:06:29 -0400

    adding-api-vercel

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <v-app>
     <!-- Criando header da aplicação -->
     <v-app-bar color="primary">
-      <v-btn icon @click.stop="toggleDrawer">
+      <v-btn icon @click.stop="drawer = !drawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
       <v-app-bar-title>Cadastro de produtos do Pedrinho</v-app-bar-title>
@@ -10,13 +10,13 @@
 
     <!-- Criando menu lateral -->
     <v-navigation-drawer v-model="drawer" color="primary">
-      <v-list-item to="Home" link prepend-icon="mdi-home-circle-outline">
+      <v-list-item to="/home" link prepend-icon="mdi-home-circle-outline">
         Home
       </v-list-item>
 
-      <v-list-item to="Products" link prepend-icon="mdi-shopping">Products</v-list-item>
+      <v-list-item to="/products" link prepend-icon="mdi-shopping">Products</v-list-item>
 
-      <v-list-item  to="Logout" link prepend-icon="mdi-logout">Logout</v-list-item>
+      <v-list-item  to="/logout" link prepend-icon="mdi-logout">Logout</v-list-item>
     </v-navigation-drawer>
 
     <!-- Criando conteúdo principal -->
@@ -34,7 +34,7 @@ import { ref } from "vue";
 const drawer = ref(false);
 
 // Função para abrir e fechar o menu lateral
-const toggleDrawer = () => {
-  drawer.value = !drawer.value;
-};
+// const toggleDrawer = () => {
+//   drawer.value = !drawer.value;
+// };
 </script>
--- src/pages/Products.vue
+++ src/pages/Products.vue
@@ -1,167 +1,148 @@
 <template>
     <v-container>
-        <!-- criando um botão para abrir o modal -->
-        <v-row>
-            <v-col class="d-flex justify-end">
-                <v-btn color="primary" @click="openDialog">
-                    Cadastrar Produto
-                </v-btn>
-            </v-col>
-        </v-row>
-
-        <!-- criar a tabela de produtos -->
-        <v-row>
-            <v-col>
-                <div class="table-container">
-                    <v-table>
-                        <thead>
-                            <tr>
-                                <th>
-                                    Nome
-                                </th>
-                                <th>
-                                    Preço
-                                </th>
-                                <th>
-                                    Ações
-                                </th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <!-- criando for para listar os produtos -->
-                            <tr v-for="(product, index) in products" key="product.id">
-                                <td>{{ product.name }}</td>
-                                <td>{{ product.price }}</td>
-
-                                <!-- criando botões de delete e edit -->
-                                <td>
-                                    <v-btn icon>
-                                        <v-icon @click="editProduct(product)">mdi-pencil</v-icon>
-                                    </v-btn>
-                                    <v-btn icon>
-                                        <v-icon @click="deleteProducts(product.id)">mdi-delete</v-icon>
-                                    </v-btn>
-                                </td>
-
-                            </tr>
-                        </tbody>
-                    </v-table>
-                </div>
-            </v-col>
-        </v-row>
-
-        <!-- criando modal -->
-        <v-dialog v-model="dialog">
-            <v-card>
-                <!-- criando titulo do card -->
-                <v-card-title>
-                    {{ isEdit ? 'Editar Produto' : 'Cadastrar Produto' }}
-                </v-card-title>
-                <!-- criando inputs de texto para o modal -->
-                <v-card-text>
-                    <v-form>
-                        <v-text-field label="Nome do produto" v-model="productName"></v-text-field>
-                        <v-text-field label="Preço do produto" v-model="productPrice"></v-text-field>
-                    </v-form>
-                </v-card-text>
-                <!-- criando botoes de ação do modal -->
-                <v-card-actions>
-                    <v-btn color="blue darken-1" @click="dialog = false"> Cancelar</v-btn>
-                    <v-btn color="blue darken-1" text @click="saveProduct">{{ isEdit ? 'Salvar' : 'Adicionar' }}</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+      <!-- criando um botão para abrir o modal -->
+      <v-row>
+        <v-col class="d-flex justify-end">
+          <v-btn color="primary" @click="openDialog">
+            Cadastrar Produto
+          </v-btn>
+        </v-col>
+      </v-row>
+  
+      <!-- criar a tabela de produtos -->
+      <v-row>
+        <v-col>
+          <div class="table-container">
+            <v-table>
+              <thead>
+                <tr>
+                  <th>Nome</th>
+                  <th>Preço</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="(product, index) in products" :key="product._id">
+                  <td>{{ product.nome }}</td>
+                  <td>{{ product.preco }}</td>
+                  <td>
+                    <v-btn icon @click="editProduct(product)">
+                      <v-icon>mdi-pencil</v-icon>
+                    </v-btn>
+                    <v-btn icon @click="deleteProduct(product._id)">
+                      <v-icon>mdi-delete</v-icon>
+                    </v-btn>
+                  </td>
+                </tr>
+              </tbody>
+            </v-table>
+          </div>
+        </v-col>
+      </v-row>
+  
+      <!-- criando modal -->
+      <v-dialog v-model="dialog">
+        <v-card>
+          <v-card-title>
+            {{ isEdit ? 'Editar Produto' : 'Cadastrar Produto' }}
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field label="Nome do produto" v-model="productName"></v-text-field>
+              <v-text-field label="Preço do produto" v-model="productPrice"></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+              <v-btn color="blue darken-1" @click="saveProduct">{{ isEdit ? 'Salvar' : 'Adicionar' }}</v-btn>
+              <v-btn color="blue darken-1" @click="dialog = false">Cancelar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
-
-
-
-</template>
-
-<script setup>
-import { ref, onMounted } from "vue";
-import axios from "axios";
-import { da } from "vuetify/locale";
-import { a } from "node_modules/unplugin-vue-router/dist/types-DBiN4-4c";
-
-// definindo os tipos das variáveis
-const dialog = ref(false);
-const products = ref([]);
-const productName = ref("");
-const productPrice = ref("");
-
-// funçao para listagem dos produtos
-const getProducts = async () => {
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from "vue";
+  import axios from "axios";
+  
+  // definindo os tipos das variáveis
+  const dialog = ref(false);
+  const isEdit = ref(false);
+  const products = ref([]);
+  const productName = ref("");
+  const productPrice = ref("");
+  const editId = ref(null);
+  
+  // funçao para listagem dos produtos
+  const getProducts = async () => {
     try {
-        const response = await axios.get("http://localhost:3000/products");
-        products.value = response.data;
+      const response = await axios.get(`https://cruddesafio-cruddesafios-projects.vercel.app/products`);
+      products.value = response.data;
     } catch (error) {
-        console.error('Erro ao buscar produtos', error);
+      console.error('Erro ao buscar produtos', error);
     }
-
-};
-
-// funçao para deleçao dos produtos
-const deleteProducts = async (id) => {
+  };
+  
+  // funçao para deleçao dos produtos
+  const deleteProduct = async (id) => {
     try {
-        await axios.delete(`http://localhost:3000/products/${id}`);
-        getProducts();
+      await axios.delete(`https://cruddesafio-cruddesafios-projects.vercel.app/products/${id}`);
+      getProducts();
     } catch (error) {
-        console.error('Erro ao deletar produto', error);
+      console.error('Erro ao deletar produto', error);
     }
-};
-
-// funçao para salvar e editar os produtos
-const saveProduct = async () => {
+  };
+  
+  // funçao para salvar e editar os produtos
+  const saveProduct = async () => {
     try {
-        if (editId.value) {
-            await axios.put(`https://localhost:3000/products/${editId.value}`, {
-                name: productName.value,
-                price: productPrice.value
-            });
-        } else {
-            await axios.post(`https://localhost:3000/products`, {
-                name: productName.value,
-                price: productPrice.value
-            });
-        }
-        getProducts();
-        dialog.value = false;
-        productName.value = '';
-        productPrice.value = '';
-        editId.value = null;
+      if (isEdit.value) {
+        await axios.put(`https://cruddesafio-cruddesafios-projects.vercel.app/products/${editId.value}`, {
+          nome: productName.value,
+          preco: productPrice.value
+        });
+      } else {
+        await axios.post(`https://cruddesafio-cruddesafios-projects.vercel.app/products`, {
+          nome: productName.value,
+          preco: productPrice.value
+        });
+      }
+      getProducts();
+      dialog.value = false;
+      productName.value = '';
+      productPrice.value = '';
+      editId.value = null;
     } catch (error) {
-        console.error('Erro ao salvar produto', error);
+      console.error('Erro ao salvar produto', error);
     }
-}
-
-// funçao para abrir o modal de edit com os dados do produto 
-const editProduct = (product) => {
+  };
+  
+  // funçao para abrir o modal de edit com os dados do produto 
+  const editProduct = (product) => {
     isEdit.value = true;
-    productName.value = product.name;
-    productPrice.value = product.price;
-    editId.value = product.id;
+    productName.value = product.nome;
+    productPrice.value = product.preco;
+    editId.value = product._id;
     dialog.value = true;
-}
-
-// funçao para abrir o modal de cadastro
-const openDialog = () => {
+  };
+  
+  // funçao para abrir o modal de cadastro
+  const openDialog = () => {
     isEdit.value = false;
     productName.value = '';
     productPrice.value = '';
     dialog.value = true;
-};
-
-// aparecer dados na tela assim que a pagina for carregada
-onMounted(getProducts)
-</script>
-
-<style>
-.table-container {
+  };
+  
+  // aparecer dados na tela assim que a pagina for carregada
+  onMounted(getProducts);
+  </script>
+  
+  <style>
+  .table-container {
     border: 1px solid #ccc;
     box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
     border-radius: 8px;
-    overflow: hidden;
-    /* Garante que o conteúdo da tabela respeite o border-radius */
-}
-</style>
+    overflow: hidden; /* Garante que o conteúdo da tabela respeite o border-radius */
+  }
+  </style>
